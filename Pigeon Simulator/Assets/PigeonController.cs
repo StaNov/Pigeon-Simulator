7ae413a93d70ac73facc98efbabf3cf74ad63251
StaNov <stanovec@gmail.com>
2015-03-29 17:46:07 +0200

    #1 otáčení holuba

--- Pigeon Simulator/Assets/PigeonController.cs
+++ Pigeon Simulator/Assets/PigeonController.cs
@@ -7,19 +7,31 @@ public class PigeonController : MonoBehaviour {
 	public Transform headTransform;
 	public Transform bodyTransform;
 	public int flightSpeed = 10;
+	public int turningSpeed = 2;
+
+	private bool rotateRight = false;
+	private bool rotateLeft = false;
 
 	void Awake () {
 		rigidbody2D = GetComponent<Rigidbody2D> ();
 	}
 	
 	void Update () {
-		
+		rotateRight = Input.GetKey (KeyCode.RightArrow);
+		rotateLeft = Input.GetKey (KeyCode.LeftArrow);
 	}
 	
 	void FixedUpdate () {
 		Vector2 directionToAddForce = headTransform.position - bodyTransform.position;
 		directionToAddForce = directionToAddForce * flightSpeed;
 		rigidbody2D.AddForce (directionToAddForce);
-		//transform.Rotate (Vector3.back);
+		
+		if (rotateRight) {
+			transform.Rotate(0, 0, -turningSpeed);
+		}
+		
+		if (rotateLeft) {
+			transform.Rotate(0, 0, turningSpeed);
+		}
 	}
 }
