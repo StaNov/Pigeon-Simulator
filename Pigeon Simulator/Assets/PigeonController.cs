daf8d2a33b2f98519d828484444fce00080a5b6f
StaNov <stanovec@gmail.com>
2015-03-29 17:55:41 +0200

    #1 chození po zemi

--- Pigeon Simulator/Assets/PigeonController.cs
+++ Pigeon Simulator/Assets/PigeonController.cs
@@ -7,10 +7,14 @@ public class PigeonController : MonoBehaviour {
 	public Transform headTransform;
 	public Transform bodyTransform;
 	public int flightSpeed = 10;
+	public int walkSpeed = 5;
 	public int turningSpeed = 2;
 
 	private bool rotateRight = false;
 	private bool rotateLeft = false;
+	private bool goForward = false;
+
+	public bool isFlying = false;
 
 	void Awake () {
 		rigidbody2D = GetComponent<Rigidbody2D> ();
@@ -19,12 +23,21 @@ public class PigeonController : MonoBehaviour {
 	void Update () {
 		rotateRight = Input.GetKey (KeyCode.RightArrow);
 		rotateLeft = Input.GetKey (KeyCode.LeftArrow);
+		goForward = Input.GetKey (KeyCode.UpArrow);
+
 	}
 	
 	void FixedUpdate () {
 		Vector2 directionToAddForce = headTransform.position - bodyTransform.position;
-		directionToAddForce = directionToAddForce * flightSpeed;
-		rigidbody2D.AddForce (directionToAddForce);
+
+		if (isFlying) {
+			Vector2 directionToAddForceFlight = directionToAddForce * flightSpeed;
+			rigidbody2D.AddForce (directionToAddForceFlight);
+		} else if (goForward) {
+			// jsem na zemi a hráč mačká šipku nahoru -> pohybuj se dopředu
+			Vector2 directionToAddForceWalk = directionToAddForce * walkSpeed;
+			rigidbody2D.AddForce (directionToAddForceWalk);
+		}
 		
 		if (rotateRight) {
 			transform.Rotate(0, 0, -turningSpeed);
