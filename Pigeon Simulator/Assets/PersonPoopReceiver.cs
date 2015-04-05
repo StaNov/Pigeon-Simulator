1714311b2d60e82003b7ea04b90d887b5c20d07b
StaNov <stanovec@gmail.com>
2015-04-05 20:47:28 +0200

    #1 ovládání kakání holuba

---
+++ Pigeon Simulator/Assets/PersonPoopReceiver.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonPoopReceiver : MonoBehaviour {
+
+	public void ReceivePoop () {
+		// TODO změn materiál na PoopedPerson
+		Debug.Log(gameObject.name + " has been pooped!");
+	}
+}
---
+++ Pigeon Simulator/Assets/PigeonPooper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PigeonPooper : MonoBehaviour {
+
+	public Transform ass;
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			// TODO pokad, co je pod tebou			
+			Collider2D collider = Physics2D.OverlapPoint(ass.position);
+
+			if (collider != null) {
+				GameObject person = collider.transform.parent.gameObject;
+
+				person.GetComponent<PersonPoopReceiver>().ReceivePoop();
+			}
+		}
+	}
+}
