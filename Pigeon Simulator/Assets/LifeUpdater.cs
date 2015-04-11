ddf8b99a4cff2d91fbff177a98a080bf6d2deae4
StaNov <stanovec@gmail.com>
2015-04-11 16:34:54 +0200

    #1 málo jídla -> ubírání života

---
+++ Pigeon Simulator/Assets/LifeUpdater.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LifeUpdater : MonoBehaviour {
+
+	private double life = 100;
+
+	public void DecreaseLife (double amount) {
+		life -= amount;
+		Render ();
+	}
+
+	private void Render() {
+		GetComponent<Text> ().text = "Život: " + Mathf.CeilToInt((float) life);
+	}
+}
