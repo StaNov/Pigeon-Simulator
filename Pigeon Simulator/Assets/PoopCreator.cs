305c7e8946b1b1337e36daf7ffaaa70e443f1d09
StaNov <stanovec@gmail.com>
2015-04-11 16:34:40 +0200

    obrázek ke čtvrtému dílu

--- Pigeon Simulator/Assets/PoopCreator.cs
+++ Pigeon Simulator/Assets/PoopCreator.cs
@@ -5,10 +5,12 @@ public class PoopCreator : MonoBehaviour {
 
 	public PoopSupplyUpdater poopUpdater;
 	public FoodUpdater foodUpdater;
+	public LifeUpdater lifeUpdater;
 	public double transformingSpeed = 0.1;
 
 	void FixedUpdate () {
 		if (foodUpdater.GetFoodAmount() < transformingSpeed) {
+			lifeUpdater.DecreaseLife(transformingSpeed);
 			return;
 		}
 
