d4c456727a226366d7fddbb09530178cb9a536db
StaNov <stanovec@gmail.com>
2015-05-03 22:10:58 +0200

    vyčištění, oddělení grafiky do zvláštní složky

---
+++ Pigeon Simulator/Assets/PeopleKiller.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PeopleKiller : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D collider) {
+		GameObject collidingObject = collider.transform.parent.parent.gameObject;
+
+		if (collidingObject.CompareTag ("person")) {
+			Destroy(collidingObject);
+		}
+	}
+}
---
+++ Pigeon Simulator/Assets/PeopleSpawner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PeopleSpawner : MonoBehaviour {
+
+	public GameObject[] spawnPoints;
+	public GameObject personPrefab;
+	public Transform peopleParent;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnPersonCoroutine());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private IEnumerator SpawnPersonCoroutine() {
+		while (true) {
+			SpawnPerson();
+			yield return new WaitForSeconds(1);
+		}
+	}
+
+	private void SpawnPerson() {
+		// vyber náhodný spawnpoint, ze kterého človíček půjde
+		GameObject fromPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+		// TODO otáčení
+		// vyber náhodný spawnpoint, do kterého človíček půjde
+		//GameObject targetPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+		// vyrob novýho človíčka ve zdrojovém spawnpointu
+		GameObject spawnedPerson = Instantiate (personPrefab);
+		spawnedPerson.transform.parent = peopleParent;
+		spawnedPerson.transform.position = fromPoint.transform.position;
+		spawnedPerson.transform.rotation = fromPoint.transform.rotation;
+
+		// ... a natoč ho směrem do cíle
+		//Debug.DrawLine (Vector3.zero, targetPoint.transform.position - fromPoint.transform.position, Color.cyan, 1);
+		//spawnedPerson.transform.LookAt(Vector3.forward,Vector3.Cross(Vector3.forward,fromPoint.transform.position - targetPoint.transform.position));
+
+	}
+}
